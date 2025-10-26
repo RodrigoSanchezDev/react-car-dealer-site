92c887d067c9d4cb937de351421d4f98404b11f5
Rodrigo Sanchez <Rodrigo@sanchezdev.com>
2025-10-26 14:45:11 -0600

    fix: add basename to Router for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/react-car-dealer-site">
       <div className="App">
         <Header />
         <main className="main-content">
